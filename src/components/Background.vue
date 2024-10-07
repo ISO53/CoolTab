d08f256e99d8327e619026db2471565e019d0d10
iso53 <102249575+ISO53@users.noreply.github.com>
2024-10-07 13:33:16 +0300

    Create Background.vue

---
+++ src/components/Background.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="background" style="background-image: 'url(${image})'; object-fit: '${objectFit}'"></div>
+</template>
+
+<script>
+export default {
+    name: "Background",
+    data() {
+        return {
+            image: null,
+            objectFit: "contain",
+        };
+    },
+};
+</script>
+
+<style scoped>
+.background {
+    width: 100vw;
+    height: 100vh;
+    margin: 0;
+    object-position: center;
+}
+</style>
