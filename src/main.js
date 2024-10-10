393e2744fbc3cdde7b5861fa27687fc15cc01ec6
iso53 <102249575+ISO53@users.noreply.github.com>
2024-10-11 01:07:50 +0300

    added v-directive for outslide click event

--- src/main.js
+++ src/main.js
@@ -7,6 +7,20 @@ import App from "./App.vue";
 
 const app = createApp(App);
 const pinia = createPinia();
+const clickOutside = {
+    beforeMount: (el, binding) => {
+        el.clickOutsideEvent = (event) => {
+            if (!(el == event.target || el.contains(event.target))) {
+                binding.value();
+            }
+        };
+        document.addEventListener("click", el.clickOutsideEvent);
+    },
+    unmounted: (el) => {
+        document.removeEventListener("click", el.clickOutsideEvent);
+    },
+};
 
+app.directive("click-outside", clickOutside);
 app.use(pinia);
 app.mount("#app");
