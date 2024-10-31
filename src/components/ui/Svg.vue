752531563062333fbdf67bc2634e175bc2148581
iso53 <102249575+ISO53@users.noreply.github.com>
2024-10-31 16:39:44 +0300

    added a svg component to use svgs easily

---
+++ src/components/ui/Svg.vue
@@ -0,0 +1,27 @@
+<template>
+    <i class="material-icons-outlined svg" draggable="false">{{ name }}</i>
+</template>
+
+<script>
+export default {
+    name: "Svg",
+    props: {
+        name: {
+            type: String,
+            required: true,
+        },
+    },
+};
+</script>
+
+<style scoped>
+.svg {
+    user-select: none;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    aspect-ratio: 1 / 1;
+    font-size: 1.8rem;
+    color: var(--color-secondary-text);
+}
+</style>
